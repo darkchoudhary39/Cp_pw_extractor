d6596d8a084832156d3536b206f2927ae0c1e0f1
darkchoudhary39 <darkchoudhary39@gmail.com>
2025-04-29 00:44:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "25431437"
 api_hash = "3b6235f4375b77e9ce448ebc3111aa50"
-bot_token = "7717267110:AAG1nwcTDra2i5yXEHbDPE6fJYkXp_yC2mc"
+bot_token = "7854852318:AAFbt9SAbtMxq0e8Pk1qHRcUBj4kBaT21HA"
 auth_users = [6434880730]
